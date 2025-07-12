ffc24e98589c91f9d104dddd16a4f0cb90467a26
christiantuyishime01 <c.tuyishime@alustudent.com>
2025-07-12 02:11:55 -0700

    This is my answer

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python3
 """
-Exports all employees' TODO tasks to a JSON file.
+Exports data of all employees' TODO lists in JSON format.
 """
 
 import json
 import requests
+import sys
 
 if __name__ == "__main__":
-    users_url = "https://jsonplaceholder.typicode.com/users"
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    url_users = "https://jsonplaceholder.typicode.com/users"
+    url_todos = "https://jsonplaceholder.typicode.com/todos"
 
-    users_response = requests.get(users_url)
-    todos_response = requests.get(todos_url)
-
-    users = users_response.json()
-    todos = todos_response.json()
+    users = requests.get(url_users).json()
+    todos = requests.get(url_todos).json()
 
     all_tasks = {}
 
     for user in users:
-    user_id = user.get("id")
-    username = user.get("username")
-    user_tasks = []
+        user_id = user.get("id")
+        username = user.get("username")
+        user_tasks = []
 
     for task in todos:
-    if task.get("userId") == user_id:
+        if task.get("userId") == user_id:
         user_tasks.append({
             "username": username,
             "task": task.get("title"),
             "completed": task.get("completed")
         })
 
-    all_tasks[str(user_id)] = user_tasks
-    with open("todo_all_employees.json", "w", encoding="utf-8") as jsonfile:
-        json.dump(all_tasks, jsonfile)
+    all_tasks[user_id] = user_tasks
+
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(all_tasks, json_file)
+                                                                                                                            
